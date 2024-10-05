bcb837be112e007247d304f07883d2811a0b7d56
Lauren Ah-Hot <laure@dhcp-vl2041-11842.eduroam.cornell.edu>
2024-10-04 22:20:26 -0400

    changes

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,12 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'dist'
-  }
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]'
+      }
+    }
+  },
+  publicDir: 'public'
 })
